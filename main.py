52f766408557ec320be2dfee0b2f6904fbaab62c
devPietroMatos <pietromatoscontato@gmail.com>
2026-02-27 23:41:04 -0300

    Menu
    First menu test

--- main.py
+++ main.py
@@ -13,12 +13,21 @@ chars_list = [['A','B','C','D','E','F','G','H','I'
             ,'s','t','u','v','w','x','y','z'], 
             ['!','@','#','$','%','&','*','/','.','_','-','?','']]
 
-tamanho_senha = int(input('Password Length: '))
+print('[ 1 ] Generate new password\n' \
+'[ 2 ] See generated passwords\n' \
+'[ 3 ] Quit')
+menu_option = int(input('Choose option: '))
 
-senha = ''
-for i in range(0, tamanho_senha):
-    sublist = random.choice(chars_list)
-    choice_char = random.choice(sublist)
-    senha  += choice_char
+if(menu_option == 1):
+    print('='*30)
 
-print(senha)
+    tamanho_senha = int(input('Password Length: '))
+    upper_chars = str
+
+    senha = ''
+    for i in range(0, tamanho_senha):
+        sublist = random.choice(chars_list)
+        choice_char = random.choice(sublist)
+        senha += choice_char
+
+    print(senha)
